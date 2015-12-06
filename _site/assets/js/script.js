faaa6d4bd2a4bb887b322f2672f8c5f5eaa0cbc8
Harrison Goldstein <hgoldstein95@gmail.com>
2015-12-06 17:29:25 -0500

    Changed color scheme around and altered nav bar

--- _site/assets/js/script.js
+++ _site/assets/js/script.js
@@ -8,6 +8,15 @@ $(document).ready(function() {
   $('.tooltipped').tooltip({
     delay: 10
   });
+  $("#side-nav").pushpin({
+    offset: 110
+  });
+  $('a[href*=#].item').click(function() {
+    $(this).addClass('active');
+    setTimeout(function() {
+      $('.active').removeClass('active');
+    }, 1500);
+  });
   $('a[href*=#]:not([href=#]):not(.c-tab)').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
